50aae39cc2a3dffe9e84ad7fc1e484d021d8d285
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-02-08 00:06:48 +0200

    Service initialized BEAN

---
+++ L05/spring-boot-intro-test/spring-boot-intro-test/src/main/java/com/example/springbootintrotest/testMe/HomeService.java
@@ -0,0 +1,17 @@
+package com.example.springbootintrotest.testMe;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeService {
+
+    private final Animal animal;
+
+    @Autowired
+    public HomeService(@Qualifier ("dog") Animal animal) {
+        this.animal = animal;
+        this.animal.makeSound();
+    }
+}
