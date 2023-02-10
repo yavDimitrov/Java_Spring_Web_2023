6f909bc97282540bab9d900d8191cd71f0486d3c
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-02-10 12:35:43 +0200

    prototype

--- L05/spring-boot-intro-test/spring-boot-intro-test/src/main/java/com/example/springbootintrotest/testMe/BeanConfiguration.java
+++ L05/spring-boot-intro-test/spring-boot-intro-test/src/main/java/com/example/springbootintrotest/testMe/BeanConfiguration.java
@@ -3,12 +3,14 @@ package com.example.springbootintrotest.testMe;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BeanConfiguration {
 
     //@Primary
     @Bean(value = "dog")
+    @Scope("prototype")
     public Animal CreateDog() {
         return new Dog();
     }
--- L05/spring-boot-intro-test/spring-boot-intro-test/src/main/java/com/example/springbootintrotest/testMe/StreetService.java
+++ L05/spring-boot-intro-test/spring-boot-intro-test/src/main/java/com/example/springbootintrotest/testMe/StreetService.java
@@ -1,28 +1,18 @@
 package com.example.springbootintrotest.testMe;
 
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-public class HomeService {
+public class StreetService {
 
     private final Animal dog;
     private final Animal cat;
 
-    private final List<Animal> animals;
-
-
-    @Autowired
-    public HomeService(Animal dog, @Qualifier ("CreateCat") Animal cat, List<Animal>animals) {
-        System.out.println("I'm in home!");
-        this.animals = animals;
-        this.cat = cat;
+    public StreetService(Animal dog, @Qualifier("CreateCat") Animal cat) {
         this.dog = dog;
-
+        this.cat = cat;
     }
 
     @PostConstruct
@@ -30,7 +20,6 @@ public class HomeService {
         System.out.println("I'm in post construct");
         this.cat.makeSound();
         this.dog.makeSound();
-        System.out.println("List of animal activities");
         System.out.println(this.cat.hashCode());
         System.out.println(this.dog.hashCode());
     }
