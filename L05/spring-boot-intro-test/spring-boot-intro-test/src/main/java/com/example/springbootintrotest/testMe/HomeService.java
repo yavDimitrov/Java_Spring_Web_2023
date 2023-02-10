f4ece29633168244ac2cca30708dd26e0248ea8a
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-02-10 11:47:23 +0200

    04 add a list

--- L05/spring-boot-intro-test/spring-boot-intro-test/src/main/java/com/example/springbootintrotest/testMe/HomeService.java
+++ L05/spring-boot-intro-test/spring-boot-intro-test/src/main/java/com/example/springbootintrotest/testMe/HomeService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class HomeService {
 
@@ -11,11 +13,12 @@ public class HomeService {
     private final Animal cat;
 
     @Autowired
-    public HomeService(Animal dog, @Qualifier ("CreateCat") Animal cat) {
+    public HomeService(Animal dog, @Qualifier ("CreateCat") Animal cat, List<Animal>animal) {
         System.out.println("I'm in home!");
         this.dog = dog;
         this.dog.makeSound();
         this.cat = cat;
         this.cat.makeSound();
+        animal.forEach(Animal::makeSound);
     }
 }
