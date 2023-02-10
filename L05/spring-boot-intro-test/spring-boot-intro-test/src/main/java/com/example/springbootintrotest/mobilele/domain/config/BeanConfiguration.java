e7910ed1a9207b93ea3943bddde58c6beea09ba7
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-02-10 14:15:31 +0200

    base entity

---
+++ L05/spring-boot-intro-test/spring-boot-intro-test/src/main/java/com/example/springbootintrotest/mobilele/domain/config/BeanConfiguration.java
@@ -0,0 +1,8 @@
+package com.example.springbootintrotest.mobilele.domain.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+}
--- L05/spring-boot-intro-test/spring-boot-intro-test/src/main/java/com/example/springbootintrotest/mobilele/domain/entities/BaseEntity.java
+++ L05/spring-boot-intro-test/spring-boot-intro-test/src/main/java/com/example/springbootintrotest/mobilele/domain/entities/BaseEntity.java
@@ -1,4 +1,28 @@
 package com.example.springbootintrotest.mobilele.domain.entities;
 
-public class BaseEntity {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.annotation.processing.Generated;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid-string")
+    @GenericGenerator(name = "uuid-string", strategy = "org.hibernate.id.uuid.UuidGenerator")
+    private String id;
+
+    public BaseEntity() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
