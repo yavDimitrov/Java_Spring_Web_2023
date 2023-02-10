9f964c9530c132a2f4d69252a4aecb7eb7943829
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-02-10 16:10:09 +0200

    starting pack of project

---
+++ L05/spring-boot-intro-test/spring-boot-intro-test/src/main/java/com/example/springbootintrotest/mobilele/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.springbootintrotest.mobilele.repositories;
+
+import com.example.springbootintrotest.mobilele.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
---
+++ L05/spring-boot-intro-test/spring-boot-intro-test/src/main/java/com/example/springbootintrotest/mobilele/repositories/UserRoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.springbootintrotest.mobilele.repositories;
+
+import com.example.springbootintrotest.mobilele.domain.entities.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRoleRepository extends JpaRepository<UserRole, String> {
+}
