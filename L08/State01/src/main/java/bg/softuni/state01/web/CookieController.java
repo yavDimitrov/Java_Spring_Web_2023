8700b83456f0108341d3308b007df1c9a16c70db
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-03-04 15:03:07 +0200

    cookie set up

--- L08/State01/src/main/java/bg/softuni/state01/web/CookieController.java
+++ L08/State01/src/main/java/bg/softuni/state01/web/CookieController.java
@@ -1,11 +1,16 @@
 package bg.softuni.state01.web;
 
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.net.http.HttpResponse;
 
 @Controller
 public class CookieController {
@@ -14,18 +19,24 @@ public class CookieController {
 
     @GetMapping("/cookies")
     public  String cookies(Model model,
-                           @CookieValue( name=LAND_COOKIE_NAME,
-                           defaultValue = "en"
+                           @CookieValue(
+                                   name=LAND_COOKIE_NAME,
+                                    defaultValue = "en"
                            ) String lang) {
-        
         model.addAttribute("lang", lang);
         return "cookies";
     }
 
-//    @PostMapping("/cookies")
-//    public String cookies() {
-//        return "redirect:/cookies";
-//    }
+    @PostMapping("/cookies")
+    public String cookies(
+            HttpServletResponse response,
+            @RequestParam("language") String language
+    ) {
+        Cookie cookie = new Cookie(LAND_COOKIE_NAME, language);
+        response.addCookie(cookie);
+
+        return "redirect:/cookies";
+    }
 
 
 }
