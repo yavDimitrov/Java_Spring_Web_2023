852be05c1b125a362f0d2fc631c5b7a2686139a1
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-03-11 12:34:00 +0200

    model added

--- L06/mobileleGO/src/main/java/bg/softuni/mobileleGO/model/entity/OfferEntity.java
+++ L06/mobileleGO/src/main/java/bg/softuni/mobileleGO/model/entity/OfferEntity.java
@@ -36,18 +36,102 @@ public class OfferEntity {
     @Column(nullable = false)
     private TransmissionEnum transmission;         // – enumerated value (MANUAL, AUTOMATIC).
 
-    private String year;         // – the year of offered car.
+    //private String year;         // – the year of offered car.
 
-    private String created ;         // – a date and time.
+    //private String created ;         // – a date and time.
 
-    private String modified;         // – a date and time.
+    //private String modified;         // – a date and time.
 
     @ManyToOne
     private ModelEntity model   ;         // – the model of a car.
 
     @ManyToOne
-    //TODO: update after seller is created!
-    private ModelEntity seller  ;         // – a user that sells the car.
+    private UserEntity seller  ;         // – a user that sells the car.
 
+    public String getId() {
+        return id;
+    }
+
+    public OfferEntity setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public EngineEnum getEngine() {
+        return engine;
+    }
+
+    public OfferEntity setEngine(EngineEnum engine) {
+        this.engine = engine;
+        return this;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public OfferEntity setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+        return this;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public OfferEntity setMileage(int mileage) {
+        this.mileage = mileage;
+        return this;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public OfferEntity setPrice(BigDecimal price) {
+        this.price = price;
+        return this;
+    }
+
+    public TransmissionEnum getTransmission() {
+        return transmission;
+    }
+
+    public OfferEntity setTransmission(TransmissionEnum transmission) {
+        this.transmission = transmission;
+        return this;
+    }
+
+    public ModelEntity getModel() {
+        return model;
+    }
+
+    public OfferEntity setModel(ModelEntity model) {
+        this.model = model;
+        return this;
+    }
+
+    public UserEntity getSeller() {
+        return seller;
+    }
+
+    public OfferEntity setSeller(UserEntity seller) {
+        this.seller = seller;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "OfferEntity{" +
+                "id='" + id + '\'' +
+                ", engine=" + engine +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", mileage=" + mileage +
+                ", price=" + price +
+                ", transmission=" + transmission +
+                ", model=" + model +
+                ", seller=" + seller +
+                '}';
+    }
 }
 
