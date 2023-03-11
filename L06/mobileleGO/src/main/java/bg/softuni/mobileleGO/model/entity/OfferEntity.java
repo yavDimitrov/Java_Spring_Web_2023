0e90cbf1afc7e356d93b48e186177495f141855e
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-03-11 14:30:50 +0200

    DB update and offers finalized

--- L06/mobileleGO/src/main/java/bg/softuni/mobileleGO/model/entity/OfferEntity.java
+++ L06/mobileleGO/src/main/java/bg/softuni/mobileleGO/model/entity/OfferEntity.java
@@ -6,6 +6,8 @@ import jakarta.persistence.*;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 
+
+
 import java.math.BigDecimal;
 import java.util.UUID;
 
@@ -13,14 +15,22 @@ import java.util.UUID;
 @Table(name="offers")
 public class OfferEntity {
 
+
+//    @Id
+//    @GeneratedValue(generator = "uuid2")
+//    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+//    @Column(name = "id", updatable = false, nullable = false, columnDefinition = "VARCHAR(36)")
+//    @Type(type = "uuid-char")
+//    private UUID id;
+
     @Id
     @GeneratedValue(generator = "UUID")
-    @GenericGenerator(
-            name ="UUID",
-            strategy = "org.hibernate.id.UUIDGenerator"
-    )
- // @Type (type = "uuid-char")
-    private String id;
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(columnDefinition = "varchar(255)")
+//    @Type(type = "uuid-char")
+    //TODO: find why it;s not working th ATtype part and i had to use columnDefinition
+    public UUID id;
+
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
@@ -32,6 +42,9 @@ public class OfferEntity {
 
     @Column(nullable = false)
     private BigDecimal price;
+
+    @Column(nullable = false)
+    private String description;
 
     @Column(nullable = false)
     private TransmissionEnum transmission;         // – enumerated value (MANUAL, AUTOMATIC).
@@ -48,11 +61,11 @@ public class OfferEntity {
     @ManyToOne
     private UserEntity seller  ;         // – a user that sells the car.
 
-    public String getId() {
+    public UUID getId() {
         return id;
     }
 
-    public OfferEntity setId(String id) {
+    public OfferEntity setId(UUID id) {
         this.id = id;
         return this;
     }
@@ -120,14 +133,24 @@ public class OfferEntity {
         return this;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public OfferEntity setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
     @Override
     public String toString() {
         return "OfferEntity{" +
-                "id='" + id + '\'' +
+                "id=" + id +
                 ", engine=" + engine +
                 ", imageUrl='" + imageUrl + '\'' +
                 ", mileage=" + mileage +
                 ", price=" + price +
+                ", description='" + description + '\'' +
                 ", transmission=" + transmission +
                 ", model=" + model +
                 ", seller=" + seller +
