bd7206c11529f82490abf1215d7ff45123be245a
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-03-12 23:51:50 +0200

    preparing database data.sql

---
+++ L06/mobileleGO/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+USE mobilele;
+
+INSERT INTO mobilele.users (id,email, first_name, last_name, image_url, is_active, password)
+VALUES (1, 'lachezar.balev@gmail.com', 'Lucho', 'Balev', null, 1, 'topsecret');
+
