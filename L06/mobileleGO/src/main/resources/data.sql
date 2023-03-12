733f193e17f422bc83e64df6d55712afee7fef53
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-03-13 00:03:16 +0200

    data set up

--- L06/mobileleGO/src/main/resources/data.sql
+++ L06/mobileleGO/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-USE mobilele;
+USE mobileleGO;
 
-INSERT INTO mobilele.users (id,email, first_name, last_name, image_url, is_active, password)
+INSERT INTO mobileleGO.users (id,email, first_name, last_name, image_url, is_active, password)
 VALUES (1, 'lachezar.balev@gmail.com', 'Lucho', 'Balev', null, 1, 'topsecret');
 
